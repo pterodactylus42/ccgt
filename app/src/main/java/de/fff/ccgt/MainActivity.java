df62981b0ab0a16ec5d36dd0047a909a74fbb801
pterodactylus42 <carmaneu@gmx.de>
2020-12-20 20:00:44 +0100

    Release 0.8

--- app/src/main/java/de/fff/ccgt/MainActivity.java
+++ app/src/main/java/de/fff/ccgt/MainActivity.java
@@ -45,6 +45,16 @@ import be.tarsos.dsp.pitch.PitchProcessor;
  * now using yin implementation
  * from tarsos dsp
  *
+ * JorenSix/TarsosDSP is licensed under the
+ * GNU General Public License v3.0
+ * Permissions of this strong copyleft license
+ * are conditioned on making available complete
+ * source code of licensed works and modifications,
+ * which include larger works using a licensed work,
+ * under the same license. Copyright and license
+ * notices must be preserved. Contributors provide
+ * an express grant of patent rights.
+ *
  */
 
 
