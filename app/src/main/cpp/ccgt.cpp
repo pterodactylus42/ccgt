fa099ef52806e3b6596bd40202f1734c71353187
pterodactylus42 <pterodactylus43@gmail.com>
2026-02-15 12:15:32 +0100

    added cpp to project

---
+++ app/src/main/cpp/ccgt.cpp
@@ -0,0 +1,17 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("ccgt");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("ccgt")
+//      }
+//    }
